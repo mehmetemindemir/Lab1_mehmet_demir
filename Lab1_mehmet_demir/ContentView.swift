b0af584792775f58f9e5cdb9be46e17b8316e226
Mehmet Emin Demir <mehmetemindemir@outlook.com>
2026-02-28 22:09:12 -0500

    Initial Commit

---
+++ Lab1_mehmet_demir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_mehmet_demir
+//
+//  Created by Mehmet Demır on 2026-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_mehmet_demir/Lab1_mehmet_demirApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_mehmet_demirApp.swift
+//  Lab1_mehmet_demir
+//
+//  Created by Mehmet Demır on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_mehmet_demirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
